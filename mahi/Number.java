5adfb9a205d7d9e944358834e6249b2f36bcdfa9
mahimachoudary <41734398+mahimachoudary@users.noreply.github.com>
2018-07-26 16:05:08 +0530

    Update and rename Ideone.java to Number.java

---
+++ mahi/Number.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Number
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		 int num;
+		 system.out.println("enter the number");
+		 num=sc.nextInt();
+		 if(num>0)
+		 {
+		 	system.out.println("num is postive");
+		 }
+		 else if(num<0)
+		 {
+		 	system.out.println("num is negative");
+		 }
+		else
+		{
+			system.out.println("num is zero");
+		}
+		
+		// your code goes here
+	}
+}
