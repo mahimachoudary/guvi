7c8cfb0834189cb9045b105fdc7cc4d638b4337a
mahimachoudary <41734398+mahimachoudary@users.noreply.github.com>
2018-08-08 12:47:26 +0530

    Update and rename Ideone.java to alphabate.java

---
+++ alphabate.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class alphabate
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char c= '*';
+		if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
+		System.out.println("c + is an alphabate");
+		else
+		System.out.println("c = is not a alphabate");
+		// your code goes here
+	}
+}
